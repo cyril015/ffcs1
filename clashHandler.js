22154bd0bad38ea80baab379709f92bcba2ff200
cyril015 <48018347+cyril015@users.noreply.github.com>
2020-05-20 14:24:38 +0530

    Add files via upload

---
+++ clashHandler.js
@@ -0,0 +1,58 @@
+  function checkClash(slots) {
+
+    var timeTableArray = $(".timeTable .slotNames").toArray();
+    var clashed = false;
+    var clashingSlots = [];
+    var currentSlot;
+    var checkLab;
+    for (var i = 0; i < slots.length; i++) {
+      for (var j = 0; j < slotArray.length; j++) {
+        if (slotArray[j] == slots[i]) {
+
+          currentSlot = slots[i];
+          //Check Lab or not
+          if (currentSlot.indexOf('L') != -1) {
+            checkLab = true;
+          } else {
+            checkLab = false;
+          }
+          //Corresponding clash
+          if ($(timeTableArray[j]).hasClass("addingDone")) {
+            $(timeTableArray[j]).addClass("clashBlink");
+            clashed = true;
+            clashingSlots.push(slots[i]);
+          }
+          //If the current slot is lab then check neighbour theory clash
+          else if (checkLab == true && $(timeTableArray[j - 13]).hasClass("addingDone")) {
+            $(timeTableArray[j - 13]).addClass("clashBlink");
+            $(timeTableArray[j]).addClass("clashBlinkPreview");
+            clashed = true;
+            clashingSlots.push(slots[i]);
+          }
+          //If the current slot is theory then check neighbour lab clash
+          else if (checkLab == false && $(timeTableArray[j + 13]).hasClass("addingDone")) {
+            $(timeTableArray[j + 13]).addClass("clashBlink");
+            $(timeTableArray[j]).addClass("clashBlinkPreview");
+            clashed = true;
+            clashingSlots.push(slots[i]);
+          }
+        }
+      }
+    }
+    if (clashed == false) {
+      return 1;
+    } else {
+      $(".clashingText").css("display", "block");
+      $(window).scrollTop($(".clashingText").offset().top);
+      $(".clashingText span").html(clashingSlots);
+      return 0;
+    }
+  }
+  $(".clashingText button").click(function() {
+    $(".clashingText").css("display", "none");
+    var timeTableArray = $(".timeTable .slotNames").toArray();
+    for (i = 0; i < timeTableArray.length; i++) {
+      $(timeTableArray[i]).removeClass("clashBlink");
+      $(timeTableArray[i]).removeClass("clashBlinkPreview");
+    }
+  });
